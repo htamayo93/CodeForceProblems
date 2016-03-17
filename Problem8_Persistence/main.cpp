b6806c448e802cfb6d6883c4f50a487832ba0e89
htamayo93 <htamayo@student.rcc.edu>
2016-03-17 15:59:21 -0700

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

---
+++ Problem8_Persistence/main.cpp
@@ -0,0 +1,48 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on March 17, 2016, 2:21 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    int time1=1;
+    int num;
+    int time2;
+    int count=0;
+    
+    cin>>num;
+    if(num<10)
+    {
+        cout<<count;
+    }
+    else
+    {
+        while(num>=10)
+        {
+          time2=num%10;
+          time1=time1*time2;
+          num=num/10;
+          if (num<10)
+          {
+              time1=num*time1;
+              num=time1;
+              time1=1;
+          }
+          count++;
+          
+        }
+        
+        cout<<count<<endl;
+    }
+    
+    
+    
+    
+    return 0;
+    
+}
+
---
+++ Problem8_Persistence/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=problem8_persistence
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/problem8_persistence
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=problem8persistence.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/problem8persistence.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=problem8_persistence
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/problem8_persistence
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=problem8persistence.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/problem8persistence.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
