dfe3c2453be034719396afd45b4cc8b93090a5eb
htamayo93 <htamayo@student.rcc.edu>
2016-03-24 15:56:50 -0700

    Signed-off-by: htamayo93 <htamayo@student.rcc.edu>

---
+++ CppApplication_2/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on March 17, 2016, 2:21 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    int time1=1;
+    int num;
+    int time2;
+    int count=0;
+    
+    cin>>num;
+    if(num<10)
+    {
+        cout<<count;
+    }
+    else
+    {
+        while(num>=10)
+        {
+          time2=num%10;
+          time1=time1*time2;
+          while(num>=10)
+          {
+              num=num/10;
+              time1=num*time1;
+              num=time1; 
+              time1=1;
+              count++;
+          }
+          count++;
+        }
+        
+        cout<<count<<endl;
+    }
+    
+    
+    return 0;
+    
+}
